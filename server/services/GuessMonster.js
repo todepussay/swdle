a9c524dc95b028348418f6a3736d77f91bc6c9db
Tom Depussay <todepussay@stpbb.org>
2024-02-23 21:45:52 +0100

    Commencement changement GuessMonster.js

--- server/services/GuessMonster.js
+++ server/services/GuessMonster.js
@@ -2,7 +2,8 @@ const { db } = require('./db');
 const { getDailyPick } = require('./DailyPick');
 
 const guessMonster = async (req, res) => {
-    const { monster_id } = req.body;
+
+    const { monster_id, number_try } = req.body;
     const dailyPick = getDailyPick().daily_monster;
 
     db.query(
@@ -29,8 +30,10 @@ const guessMonster = async (req, res) => {
                             correct: true
                         });
                     } else {
-                        res.status(200).json({
+
+                        let obj = {
                             correct: false,
+                            date: dailyPick.date,
                             information: {
                                 natural_stars: monster.natural_stars === dailyPick.natural_stars,
                                 natural_stars_more: monster.natural_stars < dailyPick.natural_stars,
@@ -42,7 +45,35 @@ const guessMonster = async (req, res) => {
                                 leader_skill: monster.leader_skill === dailyPick.leader_skill,
                                 fusion_food: monster.fusion_food === dailyPick.fusion_food
                             }
-                        })
+                        }
+
+                        switch(number_try){
+                            case 6:
+                                db.query(
+                                    `
+                                    SELECT 
+                                        s.icon_filename as image 
+                                    FROM 
+                                        skill s 
+                                    INNER JOIN monster_skill ms ON ms.skill_id = s.id 
+                                    WHERE ms.monster_id = ?
+                                    ORDER BY RAND() 
+                                    LIMIT 1
+                                    `,
+                                    [monster_id],
+                                    (err, result) => {
+                                        if(err){
+                                            console.log(err);
+                                            res.status(500).send("Internal server error");
+                                        } else {
+                                            obj.skill = result[0];
+                                            res.status(200).json(obj);
+                                        }
+                                    }
+                                )
+                        }
+
+                        // res.status(200).json()
                     }
 
                 }
